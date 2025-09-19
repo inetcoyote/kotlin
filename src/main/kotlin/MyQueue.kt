0fc546bac25b1aebe916640aaf7452d8f64a9133
engene <engene@mail.ru>
2025-09-19 14:04:16 +0300

    queue add

---
+++ src/main/kotlin/MyQueue.kt
@@ -0,0 +1,23 @@
+class MyQueue<T> {
+    private val list = mutableListOf<T>()
+
+    fun enqueue(item: T) {
+        list.add(item)
+    }
+
+    fun dequeue(): T? {
+        return if (list.isNotEmpty()) list.removeFirst() else null
+    }
+
+    fun peek(): T? {
+        return list.firstOrNull()
+    }
+
+    fun isEmpty(): Boolean {
+        return list.isEmpty()
+    }
+
+    override fun toString(): String {
+        return list.toString()
+    }
+}
